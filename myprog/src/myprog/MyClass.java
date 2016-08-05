0f62465145a11aa06f73f9fdfa15bbee4a984d55
sivangi <sivangi.sharma@hotmail.com>
2016-08-05 13:26:10 -0700

    Testing

---
+++ myprog/src/myprog/MyClass.java
@@ -0,0 +1,26 @@
+package myprog;
+
+
+
+
+public class MyClass {
+
+public static void main(String argv[]){
+	int i=9;
+	 switch (i) {
+	 default:
+		 System.out.println("default");
+	 case 0:
+	 System.out.println("zero");
+	 break;
+	 case 1:
+	 System.out.println("one");
+	 case 2:
+	 System.out.println("two");
+	 
+	 }
+
+}
+}
+	
+
