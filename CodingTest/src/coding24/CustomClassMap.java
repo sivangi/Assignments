6b0d960ecc0b81b93c9543f1ac1defb2eeb58b9f
sivangi <sivangi.sharma@hotmail.com>
2016-08-25 18:28:36 -0700

    Edited

--- CodingTest/src/coding24/CustomClassMap.java
+++ CodingTest/src/coding24/CustomClassMap.java
@@ -1,6 +1,7 @@
 package coding24;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 
 class Student {
 	private int id;
@@ -12,49 +13,73 @@ class Student {
 
 	}
 
-	 public String getStudentName() {
-	        return name;
-	 }
-	 
-	 
-	 public void setStudentName(String name) {
-	        this.name= name;
-	    }
-	 
-	    public int getStudentId(int id) {
-	        return id;
-	    }
-	 
-	 
-	 public String toString() {
+	public String getStudentName() {
+		return name;
+	}
 
-		return "The name of the student is " + name + "  the id is " + id + "";
+	public void setStudentName(String name) {
+		this.name = name;
 	}
-	 
+
+	public int getStudentId(int id) {
+		return id;
 	}
+
+	public String toString() {
+
+		return "Student is  " + name + "  the id is " + id + "";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Student other = (Student) obj;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+}
 
 public class CustomClassMap {
 
 	public static void main(String[] args) {
 
-		Student c1 = new Student (1, "Ashley");
-		Student  c2 = new Student (2, "Bob");
-		Student  c3 = new Student (3, "Carla");
-		Student c4 = new Student (4, "Dan");
-		Student c5 = new Student (5, "Elaine");
+		Student c1 = new Student(1, "Ashley");
+		Student c2 = new Student(2, "Bob");
+		Student c3 = new Student(3, "Carla");
+		Student c4 = new Student(4, "Dan");
+		Student c5 = new Student(1, "Ashley");
 
-		HashMap<Integer, Student > map = new HashMap<Integer,Student >();
-		map.put(1, c1);
-		map.put(2, c2);
-		map.put(3, c3);
-		map.put(4, c4);
-		map.put(5, c5);
+		HashMap<Student, Integer> map = new LinkedHashMap<Student, Integer>();
+		map.put(c1, 1);
+		map.put(c2, 2);
+		map.put(c3, 3);
+		map.put(c4, 4);
+		map.put(c5, 1);
 
-		for (Integer key : map.keySet()) {
-			System.out.println(key + ": " + map.get(key));
+		for (Student key : map.keySet()) {
+			System.out.println(key + ":" + map.get(key));
 		}
 
 	}
 }
-
-
